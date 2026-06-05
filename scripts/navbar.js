a52c3ec9abebcd66a7837e67acfcc6310e896972
packetFury <packetfury@pm.me>
2026-06-04 23:51:16 -0500

    Added content to landing page, modified CSS for layout and animation effects, added proper links to navbar.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,15 +2,15 @@ class Navbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="hero-nav">
-            <div class="nav-item"><h3><a href="../index.html" class="nav-link">Home</a></h3></div>
+            <div class="nav-item"><h3><a href="/index.html" class="nav-link">Home</a></h3></div>
             <div class="separator">|</div>
-            <div class="nav-item"><h3>Experience</h3></div>
+            <div class="nav-item"><h3><a href="/resume.html" class="nav-link">Experience</a></h3></div>
             <div class="separator">|</div>
-            <div class="nav-item"><h3><a href="gallery/index.html" class="nav-link">Project Gallery</a></h3></div>
+            <div class="nav-item"><h3><a href="/gallery/index.html" class="nav-link">Project Gallery</a></h3></div>
             <div class="separator">|</div>
             <div class="nav-item"><h3><a href="https://medium.com/@packetfury" class="nav-link">Blog</a></h3></div>
             <div class="separator">|</div>
-            <div class="nav-item"><h3>Contact</h3></div>
+            <div class="nav-item"><h3><a href="/contact.html" class="nav-link">Contact</a></h3></div>
         </div>
         `;
     }
